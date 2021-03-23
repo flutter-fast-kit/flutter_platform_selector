be240be95ce0171bdf926568cbd85be5a873919e
SimMan <liwei0990@gmail.com>
2021-03-23 14:25:24 +0800

    空安全支持

--- lib/src/platform_selector.dart
+++ lib/src/platform_selector.dart
@@ -1,36 +1,21 @@
 part of flutter_platform_selector;
 
 class PlatformSelector {
-  static T select<T>(
-      {T ios,
-      T android,
-      T linux,
-      T macos,
-      T windows,
-      T fuchsia,
-      T web,
-      T mobile,
-      T desktop,
-      T other}) {
+  static T? select<T>(
+      {T? ios, T? android, T? linux, T? macos, T? windows, T? fuchsia, T? web, T? mobile, T? desktop, T? other}) {
     switch (defaultTargetPlatform) {
       case TargetPlatform.iOS:
         return ios;
-        break;
       case TargetPlatform.android:
         return android;
-        break;
       case TargetPlatform.linux:
         return linux;
-        break;
       case TargetPlatform.macOS:
         return macos;
-        break;
       case TargetPlatform.windows:
         return windows;
-        break;
       case TargetPlatform.fuchsia:
         return fuchsia;
-        break;
       default:
     }
 
